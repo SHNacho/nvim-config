e5befdd56a3a83bde5ca25fe4333ce3bfd4e5460
SHNacho <nachosanchezh@hotmail.com>
2025-06-01 15:15:14 +0200

    Remove unused keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -37,16 +37,12 @@ map('n', '<F11>', '<Cmd>lua require\'dap\'.step_into()<CR>', opts)
 map('n', '<F12>', '<Cmd>lua require\'dap\'.step_out()<CR>', opts)
 map('n', '<Leader>bb', '<Cmd>lua require\'dap\'.toggle_breakpoint()<CR>', opts)
 map('n', '<Leader>B', '<Cmd>lua require\'dap\'.set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', opts)
-map('n', '<Leader>dr', '<Cmd>lua require\'dap\'.repl.open()<CR>', opts)
-map('n', '<Leader>dl', '<Cmd>lua require\'dap\'.run_last()<CR>', opts)
-map('n', '<Leader>dc', '<Cmd>lua require\'dap\'.continue()<CR>', opts)  -- Continue or start debugging
 map('n', '<Leader>ds', '<Cmd>lua require\'dap\'.terminate()<CR>', opts)  -- Stop debugging
 map('n', '<Leader>dp', '<Cmd>lua require\'dap\'.pause()<CR>', opts)      -- Pause debugging
 map('n', '<Leader>dq', '<Cmd>lua require\'dap\'.close()<CR>', opts)      -- Close the DAP session
 map('n', '<Leader>di', '<Cmd>lua require\'dap.ui.widgets\'.hover()<CR>', opts) -- Inspect
 
 -- Keybinding to select and run a DAP configuration
-map('n', '<Leader>dc', '<Cmd>lua require\'dap\'.continue()<CR>', opts)
 map('n', '<Leader>df', '<Cmd>lua require\'dap\'.run_to_cursor()<CR>', opts)
 map('n', '<Leader>dg', '<Cmd>lua require\'dap\'.run({name = vim.fn.input("Config name: ")})<CR>', opts)
 map('n', '<Leader>du', '<Cmd>lua require("dapui").toggle()<CR>', opts)
